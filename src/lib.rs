78d8a345876e452f7eb343b9b8c670bc31065de7
kangalioo <jannik.a.schaper@web.de>
2021-07-07 14:56:24 +0200

    Update crate root docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 #![warn(missing_docs)]
 
 //! This crate provides newtype wrappers around chrono's [`NaiveDateTime`], [`NaiveDate`],
-//! [`NaiveTime`], and [`Duration`] structs, that can be used in [`PyO3`] applications.
+//! [`NaiveTime`], and [`Duration`] structs, that can be used in [`PyO3`](pyo3) applications.
 //!
 //! Leap seconds are handled correctly, however timezones are not supported because Python itself
 //! doesn't inherently support timezones in its datetimes.
+//!
+//! Implementations for the [`serde::Serialize`] and [`serde::Deserialize`] traits can be enabled via the
+//! `serde` feature flag.
 //!
 //! # Truncation
 //! Python can store durations from negative one billion days up to positive one billion days long,
